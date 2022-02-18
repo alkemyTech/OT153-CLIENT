15f125aab95f685082d236a564d5fee50f6ca8bf
federicorojas11 <federico.rojas.121@gmail.com>
2022-02-18 15:26:11 -0300

    add primeng styles dependence - init primeng on app.component.ts

--- src/app/shared/primeng/primeng.module.ts
+++ src/app/shared/primeng/primeng.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ButtonModule } from 'primeng/button';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RippleModule } from "primeng/ripple";
 
-
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-  ],
-  exports: [
-    ButtonModule
-  ]
+  imports: [CommonModule],
+  exports: [ButtonModule, RippleModule, CommonModule],
 })
-export class PrimengModule { }
+export class PrimengModule {}
