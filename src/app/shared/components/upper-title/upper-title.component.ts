69893f8b437dbf05426a8db867c2123d2168927c
NLuna19 <nluna190898@gmail.com>
2022-02-18 19:14:51 -0300

    change in the component, default background image of the component for the temporary moment

--- src/app/shared/components/upper-title/upper-title.component.ts
+++ src/app/shared/components/upper-title/upper-title.component.ts
@@ -17,4 +17,16 @@ export class UpperTitleComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  backgroundSelect(): boolean{
+    return !(this.pathBackground===undefined)
+  }
+
+  background():string{
+    if (this.backgroundSelect()){
+      return "url("+this.pathBackground+")";
+    }else{
+      return "url('http://placekitten.com/400/400')";
+    }
+  }
+
 }
