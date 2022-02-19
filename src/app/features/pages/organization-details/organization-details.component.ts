cad515639f87dec503305cbda4034bb9ca1c6ff1
frangdelsolar <frangdelsolar@gmail.com>
2022-02-19 17:24:45 -0300

    OrganizationDetails logic and view updated

--- src/app/features/pages/organization-details/organization-details.component.ts
+++ src/app/features/pages/organization-details/organization-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Organization } from 'src/app/shared/models/organization.model';
 
 @Component({
   selector: 'app-organization-details',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationDetailsComponent implements OnInit {
 
+  organization: Organization; 
+
   constructor() { }
 
   ngOnInit(): void {
+    // ERASE -> Mock data until proper service is set. 
+    this.organization = {
+      "id": 1,
+      "name": "Organizacion SomosMas",
+      "logo": "http://ongapi.alkemy.org/storage/uyxA3bAbI2.jpeg",
+      "short_description": "<p>prueba prueba SDAS</p>",
+      "long_description": "prueba prueba",
+      "welcome_text": "Bienvenido a Somos Más",
+      "address": "Cra. 22 ## 80-73, Bogotá, Colombia",
+      "phone": "string",
+      "cellphone": "string",
+      "created_at": "2021-03-31T12:33:48.000000Z",
+      "updated_at": "2022-02-15T16:01:48.000000Z",
+      "facebook_url": "https://github.com/alkemyTech/OT130-CLIENT/pull/20",
+      "linkedin_url": "https://github.com/alkemyTech/OT130-CLIENT/pull/20",
+      "instagram_url": "https://github.com/alkemyTech/OT130-CLIENT/pull/20",
+      "twitter_url": "https://github.com/alkemyTech/OT130-CLIENT/pull/20"
+    }
+    // END ERASE - Mock Data
   }
 
 }
