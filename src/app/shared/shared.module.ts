882043810d1fada5e9d83524903f37039b8ac389
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2022-02-18 22:04:01 -0300

    OT153 Integration - Fixed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
-
+import { PrimengModule } from './primeng/primeng.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CKEditorModule
+    CKEditorModule,
+    PrimengModule,
+    ReactiveFormsModule
+  ],
+  exports:[
+    CKEditorModule,
+    PrimengModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
