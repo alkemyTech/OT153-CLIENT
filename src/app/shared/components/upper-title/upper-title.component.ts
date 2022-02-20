f11dbfa08726abcaee1821188994dea12589a9db
NLuna19 <nluna190898@gmail.com>
2022-02-20 16:40:26 -0300

    add input to enter classes in the component

--- src/app/shared/components/upper-title/upper-title.component.ts
+++ src/app/shared/components/upper-title/upper-title.component.ts
@@ -10,23 +10,12 @@ export class UpperTitleComponent implements OnInit {
   @Input()pathBackground!: string;
   @Input()colorBackground!: string;
   @Input()colorText!: string;
+  @Input()class!: string;
 
   constructor() {
   }
 
   ngOnInit(): void {
-  }
-
-  backgroundSelect(): boolean{
-    return !(this.pathBackground===undefined)
-  }
-
-  background():string{
-    if (this.backgroundSelect()){
-      return "url("+this.pathBackground+")";
-    }else{
-      return "url('http://placekitten.com/400/400')";
-    }
   }
 
 }
