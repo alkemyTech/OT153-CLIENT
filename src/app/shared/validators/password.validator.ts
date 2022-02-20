4b123da0f9337221fa0c13cec443c8e7c6f806cf
frangdelsolar <frangdelsolar@gmail.com>
2022-02-20 19:53:27 -0300

    PasswordValidator added

---
+++ src/app/shared/validators/password.validator.ts
@@ -0,0 +1,24 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+
+export function passwordStrengthValidator(): ValidatorFn {
+    return (control:AbstractControl) : ValidationErrors | null => {
+
+        const value = control.value;
+
+        if (!value) {
+            return null;
+        }
+
+        const hasUpperCase = /[A-Z]+/.test(value);
+
+        const hasLowerCase = /[a-z]+/.test(value);
+
+        const hasNumeric = /[0-9]+/.test(value);
+
+        const hasSymbol = /[-!$%@^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(value);
+
+        const passwordValid = (hasUpperCase || hasLowerCase) && hasNumeric && hasSymbol;
+
+        return !passwordValid ? {passwordStrength:true}: null;
+    }
+}
