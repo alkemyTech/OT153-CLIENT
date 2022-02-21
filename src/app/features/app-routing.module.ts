8826c07522deb5fbad392e13272cf6a920c0fb52
Facundo Saldivia <facundosaldivia@gmail.com>
2022-02-21 19:35:03 -0300

    dashboard component created

--- src/app/features/app-routing.module.ts
+++ src/app/features/app-routing.module.ts
@@ -2,16 +2,21 @@ import { ActivityFormComponent } from "./pages/activities/activity-form/activity
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
-import { AboutComponent } from './pages/about/about.component';
+import { AboutComponent } from "./pages/about/about.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 const routes: Routes = [
-  { 
-    path: "actividades", 
-    component: ActivityFormComponent 
+  {
+    path: "actividades",
+    component: ActivityFormComponent,
+  },
+  {
+    path: "backoffice",
+    component: DashboardComponent,
   },
   {
     path: "nosotros",
-    component: AboutComponent
+    component: AboutComponent,
   },
   {
     path: "",
