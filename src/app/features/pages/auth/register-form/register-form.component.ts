9e055db7f5140e9998fcdb1f6b2befcf82c3ba75
NLuna19 <nluna190898@gmail.com>
2022-02-21 18:45:11 -0300

    comparison of password with confirm password

--- src/app/features/pages/auth/register-form/register-form.component.ts
+++ src/app/features/pages/auth/register-form/register-form.component.ts
@@ -1,6 +1,6 @@
-import { patternValidator, passwordMatchValidator } from 'src/app/shared/validators/CustomValidator';
+import { patternValidator, passwordMatchValidator } from './CustomValidator';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -8,6 +8,8 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register-form.component.scss']
 })
 export class RegisterFormComponent implements OnInit {
+  public frmSignup: FormGroup;
+  
   private patternEmail: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //
   private patternSymbols: RegExp = /[-!$%@^&*()_+|~=`{}\[\]:";'<>?,.\/]/; //
 
@@ -23,38 +25,47 @@ export class RegisterFormComponent implements OnInit {
       patternValidator(this.patternSymbols, { hasSymbol: true })
     ] );
 
-  private confirmPasswordFormControl = new FormControl( '', [Validators.required, Validators.minLength(6)] );
+  private confirmPasswordFormControl = ([null, Validators.compose([Validators.required])]);
 
   constructor(private formBuilder: FormBuilder) { 
+    this.frmSignup = this.registerForm();
   }
 
-  registerForm = this.formBuilder.group({
-    useremail: this.useremailFormControl,
-    password: this.passwordFormControl,
-    confirmPassword: this.confirmPasswordFormControl
-  },
-  { 
-     
-  });
+  registerForm(): FormGroup {
+    return this.formBuilder.group(
+      {
+        useremail: this.useremailFormControl,
+        password: this.passwordFormControl,
+        confirmPassword: this.confirmPasswordFormControl
+     },
+     {
+        // check whether our password and confirm password match
+        validator: passwordMatchValidator
+     });
+  }
 
 
   ngOnInit(): void {
   }
 
   submit() {
-    console.log(this.registerForm.value)
+    console.log(this.registerForm().value)
+  }
+
+  get formBuild():FormBuilder{
+    return this.formBuilder as FormBuilder;
   }
 
   get useremailControl():FormControl{
-    return this.registerForm.get('useremail') as FormControl;
+    return this.frmSignup.controls['useremail'] as FormControl;
   }
 
   get passwordControl():FormControl{
-    return this.registerForm.get('password') as FormControl;
+    return this.frmSignup.controls['password'] as FormControl;
   }
 
   get confirmPasswordControl():FormControl{
-    return this.registerForm.get('confirmPassword') as FormControl;
+    return this.frmSignup.controls['confirmPassword'] as FormControl;
   }
 
 }
