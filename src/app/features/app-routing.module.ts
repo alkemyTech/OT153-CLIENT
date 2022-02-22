a7fd43748a9be73eab7d7a7549fda7363388d129
Facundo Saldivia <facundosaldivia@gmail.com>
2022-02-22 11:18:01 -0300

    resolving rebase issues

--- src/app/features/app-routing.module.ts
+++ src/app/features/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
 import { AboutComponent } from "./pages/about/about.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { DashboardComponent } from "./pages/backoffice/dashboard/dashboard.component";
 
 const routes: Routes = [
   {
