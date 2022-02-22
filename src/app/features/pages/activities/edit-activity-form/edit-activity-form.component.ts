c009d1f9f05bd7cc5cdbe6d04cb8acc709b6949a
federicorojas11 <federico.rojas.121@gmail.com>
2022-02-22 16:40:16 -0300

    config reusable form component & created edit/create components

---
+++ src/app/features/pages/activities/edit-activity-form/edit-activity-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-activity-form',
+  templateUrl: './edit-activity-form.component.html',
+  styleUrls: ['./edit-activity-form.component.scss']
+})
+export class EditActivityFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
