ee34644b8f394f7d39aa9d34313911030a8cfa69
NLuna19 <nluna190898@gmail.com>
2022-02-22 02:40:47 -0300

    implementation of enums in regex, distribution of validations. low coupling.

---
+++ src/app/core/util/validators/form.validators.ts
@@ -0,0 +1,16 @@
+import { ValidatorFn } from "@angular/forms";
+import { patternValidator } from './custom.validators';
+import { DIGIT_REGEX, WORD_REGEX, SYMBOLS_REGEX, EMAIL_REGEX } from '../../enums/regex.enum';
+
+export function digitValidator(): ValidatorFn {
+    return patternValidator( DIGIT_REGEX, { hasNumber: true });
+}
+export function wordValidator(): ValidatorFn {
+    return patternValidator( WORD_REGEX, { hasWord: true });
+}
+export function symbolValidator(): ValidatorFn{
+    return patternValidator( SYMBOLS_REGEX, { hasSymbol: true });
+}
+export function emailValidator(): ValidatorFn {
+    return patternValidator( EMAIL_REGEX, { hasEmail: true });
+}
--- src/app/core/util/validators/password.validators.ts
+++ src/app/core/util/validators/password.validators.ts
@@ -1,21 +1,4 @@
-
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
-
-
-export function patternValidator(regex: RegExp, error: ValidationErrors): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-        if (!control.value) {
-        // if control is empty return no error
-            return null;
-        }
-    
-        // test the value of the control against the regexp supplied
-        const valid = regex.test(control.value);
-    
-        // if true, return no error (no error), else return error passed in the second parameter
-        return valid ? null : error;
-    };
-}
 
 export function passwordMatchValidator(control: AbstractControl) {
     const password: string = control.get('password')!.value; // get password from our password form control
@@ -26,7 +9,3 @@ export function passwordMatchValidator(control: AbstractControl) {
         control.get('confirmPassword')!.setErrors({ NoPassswordMatch: true });
     }
 }
-
-
-
-  
