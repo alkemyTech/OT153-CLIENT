a88d73819a572ca88f0ddf551d3c97bf030dd227
frangdelsolar <frangdelsolar@gmail.com>
2022-02-23 20:14:02 -0300

    GetControl Documented

---
+++ src/app/core/util/getControlForm.ts
@@ -0,0 +1,10 @@
+import { AbstractControl, FormGroup } from '@angular/forms';
+/**
+ * Esta es una función que devuelve el controlador del campo del formulario. 
+ * @param form FormGroup
+ * @param controlName Nombre del control
+ * @returns AbstractControl || null
+ */
+export function getControl ( form: FormGroup, controlName: string ): AbstractControl | null {
+    return form.get(controlName);
+}
