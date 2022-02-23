c39bd647379d45ae2bbea95b08f0098514e4f501
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2022-02-23 20:07:12 -0300

    bug fixing

--- src/app/core/models/user.models.ts
+++ src/app/core/models/user.models.ts
@@ -1,26 +1,3 @@
-export interface User {
-    id?:                number;
-    name?:              string;
-    email?:             string;
-    email_verified_at?: string;
-    password?:          string;
-    role_id?:           number;
-    remember_token?:    string;
-    created_at?:        Date | string;
-    updated_at?:        Date | string;
-    deleted_at?:        Date | string;
-    group_id?:          number | null;
-    latitude?:          number;
-    longitude?:         number;
-    address?:           string;
-    profile_image?:     string;
-    description?:       string;
-}
-
-export interface ImgFile {
-    id: string;
-    imgFile?:string;
-}
 export interface MercadoPagoResponse {
     additional_info:      string;
     auto_return:          string;
