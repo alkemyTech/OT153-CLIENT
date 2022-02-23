cfb7c77b64927fd86c783dbc19b4be896f3b2669
federicorojas11 <federico.rojas.121@gmail.com>
2022-02-23 17:12:51 -0300

    add edit activity typescript, model & httpcall. Includes validation, spinners & http response messages

--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { ActivityResponse } from "@app/shared/models/activity.model";
 import { Observable } from "rxjs";
 
 @Injectable({
@@ -29,7 +30,6 @@ export class HttpService {
     body: any,
     activateHeader: boolean = false
   ): Observable<T> {
-    console.log(body);
     return this.http.post<T>(
       url,
       body,
@@ -42,10 +42,19 @@ export class HttpService {
     body: any,
     activateHeader: boolean = false
   ): Observable<T> {
-    console.log(body);
     return this.http.patch<T>(
       url,
       body,
+      activateHeader ? { headers: this._headers } : {}
+    );
+  }
+
+  public getActivity<T>(
+    url: string,
+    activateHeader: boolean = false
+  ): Observable<ActivityResponse> {
+    return this.http.get<ActivityResponse>(
+      url,
       activateHeader ? { headers: this._headers } : {}
     );
   }
