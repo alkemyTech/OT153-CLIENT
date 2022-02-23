febc68448fe65fade2792f952c1f7b30493a6441
NLuna19 <nluna190898@gmail.com>
2022-02-23 15:57:54 -0300

    Validation of fields in title and category

--- src/app/features/backoffice/pages/news/news-form/news-form.component.ts
+++ src/app/features/backoffice/pages/news/news-form/news-form.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-interface simpleCategory {
-  id:string;
-  name:string;
-}
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-news-form',
@@ -12,18 +8,48 @@ interface simpleCategory {
 })
 
 export class NewsFormComponent implements OnInit {
-  
-  constructor() { }
+  private frmNews: FormGroup;
+  private titleFormControl: FormControl = new FormControl( '', [ Validators.required, Validators.minLength(4) ] );
+  private contentFormControl: FormControl = new FormControl( '', [ Validators.required ] );
+  private categoryInvalid: boolean;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.frmNews = this.newsForm();
+  }
 
   ngOnInit(): void {
   }
 
   submit(): void{
-
   }
 
-  selectedIdCategory(mensaje) {
-    console.log('idCategory: ', mensaje);
+  newsForm(): FormGroup{
+    return this.formBuilder.group(
+      {
+        title: this.titleFormControl,
+        content: this.contentFormControl
+      }
+    );
+  }
+
+  selectedIdCategory(id: number) {
+    console.log('idCategory: ', id);
+  }
+
+  dropdownCategoryEmpty(flag: boolean){
+    this.categoryInvalid = flag;
+  }
+
+  get formNews():FormGroup{
+    return this.frmNews;
+  }
+
+  get titleControl():FormControl{
+    return this.formNews.controls['title'] as FormControl;
+  }
+
+  get categoryEmpty() :boolean{
+    return this.categoryInvalid; 
   }
 
 }
