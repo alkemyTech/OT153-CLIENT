41a2309f4537f60e52450b8de53c5de635119b1b
frangdelsolar <frangdelsolar@gmail.com>
2022-02-24 20:34:02 -0300

    Slides Created
    Merge Next commands to do (3 remaining commands):

---
+++ src/app/features/backoffice/pages/slides/slides-list/slides-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slides-list',
+  templateUrl: './slides-list.component.html',
+  styleUrls: ['./slides-list.component.scss']
+})
+export class SlidesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
