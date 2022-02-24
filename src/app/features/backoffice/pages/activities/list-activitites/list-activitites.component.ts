6a7fe3d32039180150bce2cdb93152ea7b8558c9
Gonzalo Moreno <morenogonza@hotmail.com>
2022-02-24 19:26:25 -0300

    added TableModule to primeng.module.ts

--- src/app/features/backoffice/pages/activities/list-activitites/list-activitites.component.ts
+++ src/app/features/backoffice/pages/activities/list-activitites/list-activitites.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-list-activitites',
   templateUrl: './list-activitites.component.html',
@@ -7,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListActivititesComponent implements OnInit {
 
+  public url = 'http://ongapi.alkemy.org/api/activities';
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
