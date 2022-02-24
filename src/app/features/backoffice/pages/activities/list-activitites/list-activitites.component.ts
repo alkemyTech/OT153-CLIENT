c056487ca6fcf9844164f55b0313727c90e1b02b
Gonzalo Moreno <morenogonza@hotmail.com>
2022-02-24 19:13:12 -0300

    added list-activities to backoffice/pages/list-activities

---
+++ src/app/features/backoffice/pages/activities/list-activitites/list-activitites.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-activitites',
+  templateUrl: './list-activitites.component.html',
+  styleUrls: ['./list-activitites.component.scss']
+})
+export class ListActivititesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
