2ae6c56e62581d18303dcabd28a4525ab67f6ef4
NLuna19 <nluna190898@gmail.com>
2022-02-24 18:15:40 -0300

    added ckeditor with validator. split functionality on component creation or editing

--- src/app/features/backoffice/pages/news/news-form/news-form.component.ts
+++ src/app/features/backoffice/pages/news/news-form/news-form.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpService } from '@app/core/services/http.service';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { New, NewData } from '@app/core/models/news.interfaces';
 
 @Component({
   selector: 'app-news-form',
@@ -8,26 +11,61 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 
 export class NewsFormComponent implements OnInit {
+  @Input() idNews: number; //idNews <= -1 --> create // idNews >= 0 --> edit
+  private isEditFlag: boolean = false;
+  private title: string = 'Crear';
+  private url: string = 'http://ongapi.alkemy.org/api/news' ;
   private frmNews: FormGroup;
   private titleFormControl: FormControl = new FormControl( '', [ Validators.required, Validators.minLength(4) ] );
   private contentFormControl: FormControl = new FormControl( '', [ Validators.required ] );
+  private categoryFormControl: FormControl = new FormControl( '', [ Validators.required ] );
   private categoryInvalid: boolean;
 
-  constructor(private formBuilder: FormBuilder) { 
-    this.frmNews = this.newsForm();
-  }
+  private classicEditor = ClassicEditor;
+  public config;
+
+
+  constructor(private httpService: HttpService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.frmNews = this.newsForm();
+    this.defineCreateOrEdit();
+    this.config = { placeholder:'Contenido' };
+    this.loadNews();
+  }
+
+  defineCreateOrEdit():void{
+    this.isEditFlag = this.idNews >= 0? true : false;
+    this.title = this.isEdit? 'Editar' : 'Crear';
   }
 
   submit(): void{
   }
 
+  getNews(id: number):void{
+    let url = `${this.url}/${id}`;
+    this.httpService.get<New>(url).subscribe((resp)=> {
+      const {success, data} = resp;
+      if(success && data.id){
+        this.frmNews.get('title')?.setValue(data.name);
+        this.frmNews.get('content')?.setValue(data.content);
+        this.frmNews.get('category')?.setValue(data.category_id);
+        console.log(data.name);
+        
+      }
+    })
+  }
+
+  loadNews():void{
+    const id = this.idNews;
+    this.isEdit? this.getNews(id) : null ;
+  }
+
   newsForm(): FormGroup{
-    return this.formBuilder.group(
-      {
+    return this.formBuilder.group( {
         title: this.titleFormControl,
-        content: this.contentFormControl
+        content: this.contentFormControl,
+        category: this.categoryFormControl
       }
     );
   }
@@ -36,20 +74,40 @@ export class NewsFormComponent implements OnInit {
     console.log('idCategory: ', id);
   }
 
-  dropdownCategoryTouchedDirty(flag: boolean){
+  dropdownCategoryTouchedDirty(flag: boolean) {
     this.categoryInvalid = flag;
   }
 
-  get formNews():FormGroup{
+  get formNews(): FormGroup {
     return this.frmNews;
   }
 
-  get titleControl():FormControl{
-    return this.formNews.controls['title'] as FormControl;
+  get titleControl(): FormControl {
+    return this.formNews.get('title') as FormControl;
   }
 
-  get categoryTouchedDirty() :boolean{
+  get contentControl(): FormControl {
+    return this.formNews.get('content') as FormControl;
+  }
+
+  get categoryControl(): FormControl{
+    return this.formNews.get('category') as FormControl;
+  }
+
+  get categoryTouchedDirty(): boolean {
     return this.categoryInvalid; 
+  }
+
+  get isEdit(): boolean{
+    return this.isEditFlag;
+  }
+
+  get pageTitle(): string {
+    return this.title;
+  }
+
+  get editor():any {
+    return this.classicEditor;
   }
 
 }
