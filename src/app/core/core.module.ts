71090dfef2de43316cf94174be718efaa4e7c839
Facundo Saldivia <facundosaldivia@gmail.com>
2022-02-25 09:51:24 -0300

    newsletter form moved to footer/components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { HttpService } from "./services/http.service";
-import { NewsletterFormComponent } from "./components/footer/newsletter-form/newsletter-form.component";
+import { NewsletterFormComponent } from "./components/footer/components/newsletter-form/newsletter-form.component";
 import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
