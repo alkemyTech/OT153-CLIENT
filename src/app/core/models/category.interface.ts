6a2a8db1a83d3ca07b66a67342c6fac419196e5f
NLuna19 <nluna190898@gmail.com>
2022-02-25 11:58:44 -0300

    edit news

--- src/app/core/models/category.interface.ts
+++ src/app/core/models/category.interface.ts
@@ -1,6 +1,8 @@
-export interface Category {
+export interface respSimpleCategory {
+    success: boolean;
+    data:    simpleCategory;
+    message: string;
 }
-
 export interface simpleCategory {
     id: number;
     name: string;
