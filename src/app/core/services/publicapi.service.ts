8fcb66fa002d238fb22e1484c56b42229ebf32fb
Facundo Saldivia <facundosaldivia@gmail.com>
2022-02-25 18:49:43 -0300

    publicApi service created and public get added

---
+++ src/app/core/services/publicapi.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PublicapiService {
+  constructor(private http: HttpClient) {}
+
+  public get<T>(url: string, id: number | null = null): Observable<T> {
+    return this.http.get<T>(`${url}/${id ? id : ""}`);
+  }
+}
