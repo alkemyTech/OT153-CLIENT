aba5245375505dc6088983fe9723e2bd816e7e0c
NLuna19 <nluna190898@gmail.com>
2022-02-25 03:35:27 -0300

    change in category-dropdown. new input() to set selected category

--- src/app/features/backoffice/pages/news/components/category-dropdown/category-dropdown.component.ts
+++ src/app/features/backoffice/pages/news/components/category-dropdown/category-dropdown.component.ts
@@ -1,35 +1,56 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { HttpService } from "@app/core/services/http.service";
-
-//!
-interface simpleCategory {
-  id: number;
-  name: string;
-}
-
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
+import { simpleCategory} from "@app/core/models/category.interface";
 @Component({
   selector: "app-category-dropdown",
   templateUrl: "./category-dropdown.component.html",
   styleUrls: ["./category-dropdown.component.scss"],
 })
 export class CategoryDropdownComponent implements OnInit {
-  url = "http://ongapi.alkemy.org/api/categories?limit=5";
+  url = "http://ongapi.alkemy.org/api/categories";
   categories: simpleCategory[];
   selectedCategory: simpleCategory;
 
   @Input() placeholder: string;
   @Input() required: boolean = false;
+  @Input() setSelectedIdCategory: number;
   @Output() emitSelect = new EventEmitter<number>();
   @Output() emitTouchedDirty = new EventEmitter<boolean>();
 
-  constructor(private HttpService: HttpService) {}
+  constructor(private HttpService: HttpService, private route: ActivatedRoute, private http: HttpClient) {
+    
+  }
 
   ngOnInit() {
+    this.getCategory()
+  }
+
+  getCategory():void{
+    if( this.setSelectedIdCategory !== undefined 
+      || this.setSelectedIdCategory !== null){
+      let id = this.setSelectedIdCategory;      
+      this.HttpService.get<simpleCategory>(`${this.url}/${id}`).subscribe(
+        (res: any) => {
+          const { data } = res;
+          this.selectedCategory = data;
+          
+        },
+        (error) => {
+          alert(error.error.message);
+        }
+      ); 
+    } 
+    this.getCategories();  
+  }
+
+  getCategories(): void{
     this.HttpService.get<simpleCategory[]>(`${this.url}`, true).subscribe(
       (res: any) => {
         const { data } = res;
         const _categories = data;
-        this.categories = [..._categories];
+        this.categories = [..._categories];        
       },
       (error) => {
         alert(error.error.message);
@@ -38,7 +59,9 @@ export class CategoryDropdownComponent implements OnInit {
   }
 
   idSelected() {
-    if (this.selectedCategory === null) {
+    console.log(this.selectedCategory);
+    
+    if (this.selectedCategory === null|| this.selectedCategory === undefined) {
       this.emitTouchedDirty.emit(true);
       this.emitSelect.emit(undefined); //???
     } else {
@@ -47,7 +70,7 @@ export class CategoryDropdownComponent implements OnInit {
   }
 
   clickedEMPTY() {
-    if (this.required || this.selectedCategory === null || this.selectedCategory === undefined ) 
+    if (this.required && (this.selectedCategory === null || this.selectedCategory === undefined)) 
     {
       this.emitTouchedDirty.emit(true);
     } else {
