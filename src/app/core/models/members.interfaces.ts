9e4af6c4697b0d7469ecec82eded700dfb958166
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2022-02-25 20:26:20 -0300

    OT-153_67 create component members list, add path in ts config and configure styles in angular json

--- src/app/core/models/members.interfaces.ts
+++ src/app/core/models/members.interfaces.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export interface MembersResponse {
   success: boolean;
   data: Members[];
@@ -15,3 +16,16 @@ export interface Members {
   deleted_at: string;
   group_id: number;
 }
+=======
+export interface Member {
+    id: number;
+    name:        string;
+    image:       string;
+    description: string;
+    facebookUrl: string;
+    linkedinUrl: string;
+    created_at:  Date;
+    updated_at:  Date;
+    deleted_at:  Date;
+  }
+>>>>>>> d9b209e (OT-153_67 create component members list, add path in ts config and configure styles in angular json)
