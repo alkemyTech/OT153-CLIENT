5bb3cf60b211f19fdb009d397d441b941690fe11
NLuna19 <nluna190898@gmail.com>
2022-02-26 18:51:39 -0300

    added method createUser

--- src/app/core/models/users.interfaces.ts
+++ src/app/core/models/users.interfaces.ts
@@ -1,31 +1,31 @@
 export interface User {
-    success: boolean;
+    success?: boolean;
     data:    UserData;
-    message: string;
+    message?: string;
 }
 
 export interface Users {
-    success: boolean;
+    success?: boolean;
     data:    UserData[];
-    message: string;
+    message?: string;
 }
 
 export interface UserData {
-    id: number,
+    id?: number,
     name: string,
     email: string,
-    email_verified_at: string, //date
+    email_verified_at?: Date, //date
     password: string,
     role_id: number,
-    remember_token: string,
-    created_at: string,  //date
-    updated_at: string,  //date
-    deleted_at: string,  //date
-    group_id: number,
-    latitude: number,
-    longitude: number,
-    address: string,
-    profile_image: string
+    remember_token?: string,
+    created_at?: Date,  //date
+    updated_at?: Date,  //date
+    deleted_at?: Date,  //date
+    group_id?: number,
+    latitude?: number,
+    longitude?: number,
+    address?: string,
+    profile_image?: string
 }
 
 
