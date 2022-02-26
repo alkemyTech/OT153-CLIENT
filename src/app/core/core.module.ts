cbedbd93ebced945d971093d468cf3fac13b88de
NLuna19 <nluna190898@gmail.com>
2022-02-26 10:38:02 -0300

    added getAllUsers and getUserById

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,14 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { HttpService } from "./services/http.service";
-import { NewsUsersService } from "./controllers/newsUsers/newsUsers.service"
+import { UserService } from "./controllers/userService/user.service"
 import { NewsletterFormComponent } from "./components/footer/components/newsletter-form/newsletter-form.component";
 import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [NewsletterFormComponent],
   imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
-  providers: [HttpService, NewsUsersService],
+  providers: [HttpService, UserService],
   exports: [NewsletterFormComponent],
 })
 export class CoreModule {}
