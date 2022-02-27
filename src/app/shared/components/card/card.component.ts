c20322e6de4b45b887c07d4d0a693ae0fe7038af
NLuna19 <nluna190898@gmail.com>
2022-02-27 16:09:02 -0300

    styles and flexibility in the card

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -8,11 +8,21 @@ import { ICard } from '@core/models/card.interfaces'
 })
 export class CardComponent implements OnInit {
   @Input() data: ICard;
+  @Input() fixedWidth?: boolean;
   @Input() class?: string;
+  @Input() width?: number = 20;
+
+  style = {}
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.fixedWidth) {
+      this.style = {
+        'width': this.width+'rem', 
+        'margin-bottom': '2em'
+      }
+    }
   }
 
 }
