d44f12a1a56c2696a5f8ef4a22236c5923da0aa8
NLuna19 <nluna190898@gmail.com>
2022-02-27 22:55:00 -0300

    update documentation

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -7,10 +7,11 @@ import { ICard } from '@core/models/card.interfaces'
  * a description and an image. If there is an error loading the image 
  * or it does not have it, it shows an empty placeholder. 
  * By default the width is set to 100% but you can set a fixed width.
- * 
- * @param data Receives type ICard. It is the information that will be show in the view @see ICard
- * @param fixedWidth Receives type boolean. It isn't required. In true it allows to define a fixed width to the card
- * @param width Receives type number. It isn't required. If 'fixedWidth' is true, this field will define the width.
+ * @see ICard
+ * @param data Receives type ICard. It is the information that will be show in the view. 
+ * @param fixedWidth Receives type boolean. It isn't required attribute. In true it allows to define a fixed width to the card.
+ * @param width Receives type number. It isn't required attribute. If 'fixedWidth' is true, this field will define the width.
+ * @param heightDescription Receives type number. It isn't required attribute. Don't require 'fixedWidth'= true, this field will define the height of the panel.
  */
 @Component({
   selector: 'app-card',
