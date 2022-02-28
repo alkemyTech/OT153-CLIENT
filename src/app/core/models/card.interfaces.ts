4b9b4f5eb25ca0d9b85dfdeaaf42b38bc749f5bb
NLuna19 <nluna190898@gmail.com>
2022-02-27 22:24:25 -0300

    documentation of ICard

--- src/app/core/models/card.interfaces.ts
+++ src/app/core/models/card.interfaces.ts
@@ -1,5 +1,12 @@
+/**
+ *  @param name :string --> card title
+ *  @param description :string --> card description
+ *  @param image :string  --> image url 
+ *  @param altText ?:string --> alt text to be displayed if there is no image. It is not required
+ */
 export interface ICard {
     name: string,
     description: string,
-    image: string
+    image: string | undefined,
+    altText?: string,
 }
