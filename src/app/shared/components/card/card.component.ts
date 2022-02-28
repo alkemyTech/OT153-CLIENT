beab3ef7969c3f2bb005266a0b7aa7346a07076c
NLuna19 <nluna190898@gmail.com>
2022-02-27 22:28:34 -0300

    Documentation of card.component

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,6 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ICard } from '@core/models/card.interfaces' 
 
+/**
+ * @description 
+ * Reusable card component, Receives an interface to display a title, 
+ * a description and an image. If there is an error loading the image 
+ * or it does not have it, it shows an empty placeholder. 
+ * By default the width is set to 100% but you can set a fixed width.
+ * 
+ * @param data Receives type ICard. It is the information that will be show in the view @see ICard
+ * @param fixedWidth Receives type boolean. It isn't required. In true it allows to define a fixed width to the card
+ * @param width Receives type number. It isn't required. If 'fixedWidth' is true, this field will define the width.
+ */
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -9,7 +20,6 @@ import { ICard } from '@core/models/card.interfaces'
 export class CardComponent implements OnInit {
   @Input() data: ICard;
   @Input() fixedWidth?: boolean;
-  @Input() class?: string;
   @Input() width?: number = 20;
 
   style = {}
@@ -20,9 +30,11 @@ export class CardComponent implements OnInit {
     if (this.fixedWidth) {
       this.style = {
         'width': this.width+'rem', 
-        'margin-bottom': '2em'
+        'margin-bottom': '2em',
       }
-    }
+    };
   }
+
+  
 
 }
