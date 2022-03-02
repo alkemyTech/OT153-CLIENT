a75e42758abd511708316e72207b985ebc79529e
federicorojas11 <federico.rojas.121@gmail.com>
2022-03-01 21:52:00 -0300

    rebase minor fixes

--- src/app/features/public/pages/contact/contact.component.ts
+++ src/app/features/public/pages/contact/contact.component.ts
@@ -1,30 +1,32 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { Organization } from "@app/core/models/organization.interfaces";
-import { HttpService } from "@app/core/services/http.service";
-import { PublicapiService } from "@app/core/services/publicapi.service";
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Organization } from '@app/core/models/organization.interfaces';
+import { HttpService } from '@app/core/services/http.service';
+import { PublicapiService } from '@app/core/services/publicApi.service';
 
 @Component({
-  selector: "app-contact",
-  templateUrl: "./contact.component.html",
-  styleUrls: ["./contact.component.scss"],
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-  title: string = "Contactate con nosotros";
-  colorBackground: string = "transparent";
-  organization_link:string = 'api/organization'
-  organization_info:Organization 
-  constructor(public http:PublicapiService) {}
-
+  title: string = 'Contactate con nosotros';
+  colorBackground: string = 'transparent';
+  organization_link: string = 'api/organization';
+  organization_info: Organization;
+  constructor(public http: PublicapiService) {}
 
   ngOnInit(): void {
-    this.getContactInfo()
+    this.getContactInfo();
   }
 
-  getContactInfo():void{
-    this.http.get<Organization>(this.organization_link).subscribe((res)=>{
-  
-      this.organization_info = res
-    },(error)=>{alert(error)})
+  getContactInfo(): void {
+    this.http.get<Organization>(this.organization_link).subscribe(
+      (res) => {
+        this.organization_info = res;
+      },
+      (error) => {
+        alert(error);
+      }
+    );
   }
 }
