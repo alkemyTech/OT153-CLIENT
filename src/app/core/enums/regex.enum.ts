f3959f78b9f347ecf71cc29ef3788608edbe2a7d
Facundo Saldivia <facundosaldivia@gmail.com>
2022-03-02 18:02:06 -0300

    URL_HTTPS regex created

--- src/app/core/enums/regex.enum.ts
+++ src/app/core/enums/regex.enum.ts
@@ -4,6 +4,7 @@ export enum regex {
   WORD_REGEX = '[a-zA-Z]',
   SYMBOLS_REGEX = '[^a-z0-9]',
   ALLDIGITS_REGEX = '^[0-9]+$',
+  URL_HTTPS = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?',
 }
 
 export const EMAIL_REGEX: RegExp = new RegExp(regex.EMAIL_REGEX);
@@ -11,3 +12,4 @@ export const SYMBOLS_REGEX: RegExp = new RegExp(regex.SYMBOLS_REGEX);
 export const DIGIT_REGEX: RegExp = new RegExp(regex.DIGIT_REGEX);
 export const WORD_REGEX: RegExp = new RegExp(regex.WORD_REGEX);
 export const ALLDIGIT_REGEX: RegExp = new RegExp(regex.ALLDIGITS_REGEX);
+export const URL_HTTPS_REGEX:RegExp = new RegExp(regex.URL_HTTPS)
