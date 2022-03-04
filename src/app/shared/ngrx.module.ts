d29d1ad3ba9db84d085f49947141a749ddf8d163
NLuna19 <nluna190898@gmail.com>
2022-03-04 15:37:10 -0300

    added action, reducer, selector and index

--- src/app/shared/ngrx.module.ts
+++ src/app/shared/ngrx.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { fromRoot } from '@app/shared/store/indexActivities';
+import { fromRoot } from '@app/shared/store/activities.index';
 
 @NgModule({
   imports: [
