176d24b5bcb3c3aa2edd8ca3f11274267c7a90f5
NLuna19 <nluna190898@gmail.com>
2022-03-04 15:38:22 -0300

    redux implementations in list-activities

--- src/app/features/backoffice/pages/activities/list-activitites/list-activitites.component.ts
+++ src/app/features/backoffice/pages/activities/list-activitites/list-activitites.component.ts
@@ -1,6 +1,13 @@
+import { mergeMap } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { getAllActivities } from '@app/shared/store/activities.actions';
+import { activitiesState } from '@app/shared/store/activities.reducer';
+import { Observable, Subscription } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Activities, ActivitiesResponse } from '@app/core/models/activities.interfaces';
 import { PrivateService } from '@features/services/private.service';
+import { Store } from '@ngrx/store';
+import { fromRoot } from '@app/shared/store/indexActivities'; 
 
 @Component({
   selector: 'app-list-activitites',
@@ -9,13 +16,21 @@ import { PrivateService } from '@features/services/private.service';
 })
 export class ListActivititesComponent implements OnInit {
   public url = 'http://ongapi.alkemy.org/api/activities';
-  public activities: Activities[];
+  public activities: Activities[] = [];
+  public activities$ : Activities[] = [];
 
-  constructor(private privateService: PrivateService) {}
+  constructor(private privateService: PrivateService, private activityStore: Store<{ activitiesState: activitiesState }>) {}
 
   ngOnInit(): void {
-    this.privateService.get<ActivitiesResponse>(this.url).subscribe((resp) => {
-      this.activities = resp.data;
+    this.getAllActivities();
+  }
+
+
+  getAllActivities(){
+    this.activityStore.dispatch(fromRoot.getAllActivities())
+    this.activityStore.subscribe(resp => {
+      resp.activitiesState.response.data?.map(a => this.activities$.push(a) );
     });
   }
+
 }
