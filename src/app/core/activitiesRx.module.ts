79915f448745395b6b543d996c8ba0e000c5f767
NLuna19 <nluna190898@gmail.com>
2022-03-04 20:28:32 -0300

    change modules

--- src/app/core/activitiesRx.module.ts
+++ src/app/core/activitiesRx.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { fromRoot } from '@app/shared/store/activities.index';
+import { fromRoot } from '@app/core/store/activities/activities.index';
 
 @NgModule({
   imports: [
@@ -16,4 +16,4 @@ import { fromRoot } from '@app/shared/store/activities.index';
   ],
   declarations: []
 })
-export class NgrxModule { }
+export class ActivitiesRxModule { }
