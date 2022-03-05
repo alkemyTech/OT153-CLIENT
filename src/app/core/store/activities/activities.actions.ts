252279dc590978ce87b75906f9fa068fdca598db
NLuna19 <nluna190898@gmail.com>
2022-03-05 16:55:04 -0300

    implements actions, reducer and effects (all CRUD)

--- src/app/core/store/activities/activities.actions.ts
+++ src/app/core/store/activities/activities.actions.ts
@@ -1,20 +1,42 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Delete } from '@app/core/models/delete.interface';
-import { ActivitiesResponse, ActivityResponse, NewActivity } from '@core/models/activities.interfaces';
+import { Activities, NewActivity } from '@core/models/activities.interfaces';
 import { createAction, props } from '@ngrx/store';
 
-export const getAllActivities = createAction('[Activities] GetAll');
-export const getOneActivities = createAction('[Activities] GetOne', props<{id: number}>());
-export const insertActivities = createAction('[Activities] Insert', props<{body: NewActivity}>());
-export const updateActivities = createAction('[Activities] Update', props<{id: number, body: NewActivity}>());
-export const deleteActivities = createAction('[Activities] Delete', props<{id: number}>());
-export const error = createAction('[Activities] Error');
+export enum type {
+    GET_ALL_ACTIVITIES = '[Activities] Get All Activities',
+    GET_ALL_ACTIVITIES_FAIL = '[Activities] Get All Activities Fail',
+    GET_ALL_ACTIVITIES_SUCCESS = '[Activities] Get All Activities Success',
+    GET_ONE_ACTIVITY = '[Activities] Get One Activities',
+    GET_ONE_ACTIVITY_FAIL = '[Activities] Get One Activities Fail',
+    GET_ONE_ACTIVITY_SUCCESS = '[Activities] Get One Activities Success',
+    INSERT_ACTIVITY = '[Activities] Insert Activity',
+    INSERT_ACTIVITY_FAIL = '[Activities] Insert Activity Fail',
+    INSERT_ACTIVITY_SUCCESS = '[Activities] Insert Activity Success',
+    UPDATE_ACTIVITY = '[Activities] Update Activity',
+    UPDATE_ACTIVITY_FAIL = '[Activities] Update Activity Fail',
+    UPDATE_ACTIVITY_SUCCESS = '[Activities] Update Activity Success',
+    DELETE_ACTIVITY = '[Activities] Delete Activity',
+    DELETE_ACTIVITY_FAIL = '[Activities] Delete Activity Fail',
+    DELETE_ACTIVITY_SUCCESS = '[Activities] Delete Activity Success',
+}
 
-//effects
-export const responseGetAll = createAction('[Activities] Get All Response', props<{ response: ActivitiesResponse }>());
-export const responseGetOne = createAction('[Activities] Get One Response', props<{ response: ActivityResponse }>());
-export const responseInsert = createAction('[Activities] Insert Response' , props<{ response: ActivityResponse }>());
-export const responseUpdate = createAction('[Activities] Update Response' , props<{ response: ActivityResponse }>());
-export const responseDelete = createAction('[Activities] Delete Response' , props<{ response: Delete }>());
-export const responseError  = createAction('[Activities] Error Response'  , props<{ response: HttpErrorResponse }>());
+export const getAllActivities = createAction(type.GET_ALL_ACTIVITIES);
+export const GetAllActivities_Fail= createAction(type.GET_ALL_ACTIVITIES_FAIL, props<{ error: any }>());
+export const GetAllActivities_Success = createAction(type.GET_ALL_ACTIVITIES_SUCCESS, props<{ activities: Array<Activities> }>());
 
+export const getOneActivities = createAction(type.GET_ONE_ACTIVITY, props<{id: number}>());
+export const GetOneActivity_Fail= createAction(type.GET_ONE_ACTIVITY_FAIL, props<{ error: any }>());
+export const GetOneActivity_Success = createAction(type.GET_ALL_ACTIVITIES_SUCCESS, props<{ activity: Activities }>());
+
+export const insertActivities = createAction(type.INSERT_ACTIVITY, props<{body: Activities}>());
+export const InsertActivities_Fail = createAction(type.INSERT_ACTIVITY_FAIL , props<{ error: any }>());
+export const InsertActivities_Success = createAction(type.INSERT_ACTIVITY_SUCCESS , props<{ activity: Activities }>());
+
+export const updateActivities = createAction(type.UPDATE_ACTIVITY, props<{id: number, body: Activities}>());
+export const UpdateActivities_Fail = createAction(type.UPDATE_ACTIVITY_FAIL, props<{ error: any }>());
+export const UpdateActivities_Success = createAction(type.UPDATE_ACTIVITY_SUCCESS, props<{ activity: Activities }>());
+
+export const deleteActivities = createAction(type.DELETE_ACTIVITY, props<{id: number}>());
+export const DeleteActivity_Fail  = createAction(type.DELETE_ACTIVITY_FAIL, props<{ error: any }>());
+export const DeleteActivity_Success = createAction(type.DELETE_ACTIVITY_SUCCESS, props<{ delete: Delete }>());
