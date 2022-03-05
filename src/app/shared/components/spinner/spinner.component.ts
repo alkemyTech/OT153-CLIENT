5041a16687b673d193b2262f9944147e8c78d489
frangdelsolar <frangdelsolar@gmail.com>
2022-03-05 11:46:07 -0300

    Spinner view and logic implementation

--- src/app/shared/components/spinner/spinner.component.ts
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'alk-spinner',
+  selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent implements OnInit {
+
+  @Input() strokeWidth!: string; // Width of the circle stroke.
+  @Input() fillColor!: string; // Color for the background of the circle.
+  @Input() animationDuration!: string; // Duration of the rotate animation.
+  @Input() class!: string;
+  @Input() height!: number;
+  @Input() width!: number;
+  @Input() opacity!: number;
 
   constructor() { }
 
