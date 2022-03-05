d6057f790a385214fb09e4a5e094d365033b6afe
NLuna19 <nluna190898@gmail.com>
2022-03-05 16:55:04 -0300

    implements actions, reducer and effects (all CRUD)

--- src/app/core/store/activities/activities.reducer.ts
+++ src/app/core/store/activities/activities.reducer.ts
@@ -1,35 +1,41 @@
-import { responseUpdate, responseError, responseDelete, responseGetAll, responseGetOne, responseInsert } from '@app/core/store/activities/activities.actions';
+import { deleteActivities, DeleteActivity_Success, GetAllActivities_Success, GetOneActivity_Success, InsertActivities_Success, UpdateActivities_Success, GetAllActivities_Fail, GetOneActivity_Fail, DeleteActivity_Fail, InsertActivities_Fail, UpdateActivities_Fail } from './activities.actions';
+import {  } from '@app/core/store/activities/activities.actions';
 import { HttpErrorResponse } from '@angular/common/http';
-import { ActivitiesResponse, ActivityResponse } from '@app/core/models/activities.interfaces';
+import { Activities, ActivitiesResponse, ActivityResponse } from '@app/core/models/activities.interfaces';
 import { Delete } from '@app/core/models/delete.interface';
 import { createReducer, on } from '@ngrx/store';
 
 export interface activitiesState {
-    responseAll : ActivitiesResponse;
-    response : ActivityResponse;
+    responseAll : Array<Activities>;
+    response : Activities;
     delete : Delete;
     error : HttpErrorResponse;
 };
 
-const emptyResponse = { success: false , data: undefined, message: '' };
-const emptyDelete= { success: false , data: '', message: '' }
-const errorEmpty= new HttpErrorResponse({})
+const emptyArray= Array<Activities>();
+const emptyOne=  {id: 0, name: '', description:''};
+const emptyDelete= { success: false , data: '', message: '' };
+const errorEmpty= new HttpErrorResponse({});
 
 const initialState: activitiesState = {
-  responseAll: emptyResponse,
-  response: emptyResponse,
+  responseAll: emptyArray,
+  response: emptyOne,
   delete: emptyDelete,
   error: errorEmpty,
 };
  
 const _activityReducer = createReducer(
   initialState,
-  on( responseGetAll, (state, action) => ( { responseAll : action.response, response: emptyResponse, delete: emptyDelete, error: errorEmpty } ) ),
-  on( responseGetOne, (state, action) => ( { responseAll : emptyResponse, response: action.response, delete: emptyDelete, error: errorEmpty } ) ),
-  on( responseInsert, (state, action) => ( { responseAll : emptyResponse, response: action.response, delete: emptyDelete, error: errorEmpty } ) ),
-  on( responseUpdate, (state, action) => ( { responseAll : emptyResponse, response: action.response, delete: emptyDelete, error: errorEmpty } ) ),
-  on( responseDelete, (state, action) => ( { responseAll : emptyResponse, response: emptyResponse, delete: action.response, error: errorEmpty } ) ),
-  on( responseError, (state, action) => ( { responseAll : emptyResponse, response: emptyResponse, delete: emptyDelete, error: action.response } ) ),
+  on( GetAllActivities_Success, (state, action) => ( { responseAll : action.activities, response: emptyOne, delete: emptyDelete, error: errorEmpty } ) ),
+  on( GetAllActivities_Fail, (state, action) => ( { responseAll : emptyArray, response: emptyOne, delete: emptyDelete, error: action.error } ) ),
+  on( GetOneActivity_Success, (state, action) => ( { responseAll : emptyArray, response: action.activity, delete: emptyDelete, error: errorEmpty } ) ),
+  on( GetOneActivity_Fail, (state, action) => ( { responseAll : emptyArray, response: emptyOne, delete: emptyDelete, error: action.error } ) ),
+  on( InsertActivities_Success, (state, action) => ( { responseAll : emptyArray, response: action.activity, delete: emptyDelete, error: errorEmpty } ) ),
+  on( InsertActivities_Fail, (state, action) => ( { responseAll : emptyArray, response: emptyOne, delete: emptyDelete, error: action.error } ) ),
+  on( UpdateActivities_Success, (state, action) => ( { responseAll : emptyArray, response: action.activity, delete: emptyDelete, error: errorEmpty } ) ),
+  on( UpdateActivities_Fail, (state, action) => ( { responseAll : emptyArray, response: emptyOne, delete: emptyDelete, error: action.error } ) ),
+  on( DeleteActivity_Success, (state, action) => ( { responseAll : emptyArray, response: emptyOne, delete: action.delete, error: errorEmpty } ) ),
+  on( DeleteActivity_Fail, (state, action) => ( { responseAll : emptyArray, response: emptyOne, delete: emptyDelete, error: action.error } ) ),
 
 );
  
