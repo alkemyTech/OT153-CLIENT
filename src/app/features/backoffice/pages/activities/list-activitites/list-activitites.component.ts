f44dcacfe2925d2f4528d258480a7d02ac78078a
NLuna19 <nluna190898@gmail.com>
2022-03-06 14:08:31 -0300

    list-activities with redux, completed

--- src/app/features/backoffice/pages/activities/list-activitites/list-activitites.component.ts
+++ src/app/features/backoffice/pages/activities/list-activitites/list-activitites.component.ts
@@ -4,7 +4,10 @@ import { PrivateService } from '@features/services/private.service';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
 import { activitiesState } from '@app/core/store/activities/activities.reducer';
-import { fromRoot } from '@app/core/store/activities/activities.index'; 
+import { fromRoot } from '@app/core/store/activities/activities.index';
+import * as actions from '@app/core/store/activities/activities.actions';
+import { Observable } from 'rxjs';
+import { SelectStateAllData } from '@app/core/store/activities/activities.selector';
 
 @Component({
   selector: 'app-list-activitites',
@@ -14,7 +17,8 @@ import { fromRoot } from '@app/core/store/activities/activities.index';
 export class ListActivititesComponent implements OnInit {
   public url = 'http://ongapi.alkemy.org/api/activities';
   public activities: Activities[] = [];
-  public activities$ : Activities[] = [];
+
+  public activities$: Observable<any> = new Observable();
 
   constructor(private privateService: PrivateService, private activityStore: Store<{ activitiesState: activitiesState }>) {}
 
@@ -24,10 +28,20 @@ export class ListActivititesComponent implements OnInit {
 
 
   getAllActivities(){
-    this.activityStore.dispatch(fromRoot.getAllActivities())
-    this.activityStore.subscribe(resp => {
-      resp.activitiesState.responseAll.data?.map( (a: Activities) => this.activities$.push(a) );
-    });
+    
+    this.activities$ = this.activityStore.select(SelectStateAllData);
+
+    this.activityStore.dispatch(fromRoot.getAllActivities());
+
+    // this.activities$ = this.activityStore.select(fromRoot.getAllActivities)
+
+    // this.activityStore.subscribe(resp => {
+    //   resp.activitiesState.responseAll.map( (a) =>  console.log(a) );
+    //   console.log(resp.activitiesState)
+    // });
+
+    // this.activities$.subscribe( resp => console.log('resp: ', resp) )
+
   }
 
 }
