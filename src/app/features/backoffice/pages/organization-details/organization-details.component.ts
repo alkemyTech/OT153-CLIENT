2388cf28106a73c13e34c1fb7566549b364c488b
federicorojas11 <federico.rojas.121@gmail.com>
2022-03-07 17:29:04 -0300

    removed mock data  on organization-details (backoffice) -> replaced with api call

--- src/app/features/backoffice/pages/organization-details/organization-details.component.ts
+++ src/app/features/backoffice/pages/organization-details/organization-details.component.ts
@@ -14,37 +14,16 @@ export class OrganizationDetailsComponent implements OnInit {
   constructor(private httpSvc: HttpService) {}
 
   ngOnInit(): void {
-    // this.getOrganization(); //--> DISABLED UNTIL SERVICES WORK CORRECTLY
-
-    // MOCK DATA TO DELETE WHEN SERVICES IS WORKING
-    this.organization = {
-      id: 1,
-      name: 'Organizacion SomosMas',
-      logo: 'http://ongapi.alkemy.org/storage/uyxA3bAbI2.jpeg',
-      short_description: '<p>prueba prueba SDAS</p>',
-      long_description: 'prueba prueba',
-      welcome_text: 'Bienvenido a Somos Más',
-      address: 'Cra. 22 ## 80-73, Bogotá, Colombia',
-      phone: 'string',
-      cellphone: 'string',
-      created_at: '2021-03-31T12:33:48.000000Z',
-      updated_at: '2022-02-15T16:01:48.000000Z',
-      facebook_url: 'https://github.com/alkemyTech/OT130-CLIENT/pull/20',
-      linkedin_url: 'https://github.com/alkemyTech/OT130-CLIENT/pull/20',
-      instagram_url: 'https://github.com/alkemyTech/OT130-CLIENT/pull/20',
-      twitter_url: 'https://github.com/alkemyTech/OT130-CLIENT/pull/20',
-    };
-    // END MOCK DATA
+    this.getOrganization();
   }
 
   getOrganization() {
     this.httpSvc.get<Organization>(environment.apiUrlOrganization, true).subscribe(
       (res) => {
-        this.organization = res;
-        console.log(res);
+        this.organization = res.data;
       },
       (error) => {
-        console.log(error);
+        // console.log(error);
       }
     );
   }
