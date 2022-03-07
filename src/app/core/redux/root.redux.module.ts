2ecae415b435bfbc34d34d01c8df79aef19302be
NLuna19 <nluna190898@gmail.com>
2022-03-07 18:01:25 -0300

    fix root.module

--- src/app/core/redux/root.redux.module.ts
+++ src/app/core/redux/root.redux.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivitiesRxModule } from './activities/redux.activities.module';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { fromRoot as activitiesRoot} from './activities/activities.index';
 
 @NgModule({
   imports: [
     CommonModule,
-    ActivitiesRxModule,
+    StoreModule.forRoot({ 
+      activitiesState: activitiesRoot.activityReducer,
+
+    }),
+    EffectsModule.forRoot([
+      activitiesRoot.ActivitiesEffects,
+
+    ]),
     StoreDevtoolsModule.instrument({
       name: 'REDUX STATES - devtools',
       maxAge: 25, // Retains last 25 states
