4a93d4d9872b38e65c2e9c9944a19b0ab1404aba
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2022-03-07 19:19:10 -0300

    Add NgRx

--- src/app/features/backoffice/pages/auth/login-form/login-form.component.ts
+++ src/app/features/backoffice/pages/auth/login-form/login-form.component.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { getControl as getControlFunction } from '@app/core/util/getControlForm';
 import { emailValidator } from '@app/core/util/validators/form.validators';
-
+import { select, Store } from '@ngrx/store';
+import { AuthState } from '@core/redux/reducers/auth.reducers';
+import { Observable } from 'rxjs';
+import { getAuth } from '@core/redux/selectors/auth.selectors';
+import { login } from '@app/core/redux/actions/auth.actions';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
+  authentication$: Observable<boolean>;
+
   getControl = getControlFunction;
   loginForm = this.fb.group({
     email: ['', [Validators.required, emailValidator()]],
     password: ['', [Validators.required]],
   });
-
-  constructor(private fb: FormBuilder) {}
+  
+  constructor(private fb: FormBuilder, private _store:Store<AuthState>,private _router:Router) {
+    this.authentication$ = this._store.pipe(select(getAuth));
+  }
 
   ngOnInit(): void {}
 
@@ -23,5 +32,13 @@ export class LoginFormComponent implements OnInit {
     const formValue = this.loginForm.value;
     localStorage.setItem('email', formValue.email);
     localStorage.setItem('password', formValue.password);
+    const logAction = { email: this.loginForm.get('email')!.value, password: this.loginForm.get('password')!.value};
+    this._store.dispatch(login(logAction));
+
+    this.authentication$.subscribe( auth => {
+      if(auth){
+        this._router.navigate(["home"]);
+      }
+    });
   }
 }
