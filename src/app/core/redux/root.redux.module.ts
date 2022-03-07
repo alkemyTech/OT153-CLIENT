2803cd6f94056e6b43973c89940c60bbb39125dc
Gonzalo Moreno <morenogonza@hotmail.com>
2022-03-07 18:05:08 -0300

    added category redux - added category service

--- src/app/core/redux/root.redux.module.ts
+++ src/app/core/redux/root.redux.module.ts
@@ -4,9 +4,12 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { ActivitiesReducer, ActivitiesEffects } from './activities/activities.index';
+import {UserReducer, UsersEffects} from './users/user.index'
 import { categoriesReducer } from './categories/categories.reducer';
 import { CategoriesEffects } from './categories/categories.effects';
-import { UserReducer, UsersEffects } from './users/user.index';
+
+
+
 
 @NgModule({
   imports: [
@@ -14,9 +17,13 @@ import { UserReducer, UsersEffects } from './users/user.index';
     StoreModule.forRoot({
       activitiesState: ActivitiesReducer.activityReducer,
       listCategories: categoriesReducer,
-      userState: UserReducer.userReducer,
+      userState: UserReducer.userReducer
     }),
-    EffectsModule.forRoot([ActivitiesEffects, CategoriesEffects, UsersEffects]),
+    EffectsModule.forRoot([
+      ActivitiesEffects,
+      CategoriesEffects,
+      UsersEffects
+    ]),
     StoreDevtoolsModule.instrument({
       name: 'REDUX STATES - devtools',
       maxAge: 25, // Retains last 25 states
