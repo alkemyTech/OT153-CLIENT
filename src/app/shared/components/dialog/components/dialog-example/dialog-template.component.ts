0e3694719f77404cc9317bcc91b8f9805a1ba256
NLuna19 <nluna190898@gmail.com>
2022-03-09 01:29:01 -0300

    created dialog component and implemented in app.component

---
+++ src/app/shared/components/dialog/components/dialog-example/dialog-template.component.ts
@@ -0,0 +1,31 @@
+import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import {MatDialog } from '@angular/material/dialog';
+import { DialogData } from '@core/models/dialog.inteface';
+import { DialogService } from '@app/core/services/dialog.service';
+import { DialogType } from '@core/enums/dialog.enum';
+
+@Component({
+  selector: 'dialog-example',
+  templateUrl: 'dialog-template.component.html',
+})
+export class DialogExampleComponent{
+  public data$: Observable<DialogData>;
+  public dialogData: DialogData;
+  public type: DialogType;
+  public cancel = DialogType.CANCEL;
+  
+  constructor(public dialog: MatDialog, ds: DialogService) {
+    this.data$ = ds.DialogDataObservable;
+    this.data$.subscribe({
+      next: (data:DialogData) => {
+        this.dialogData = data;
+        this.type = data.type;
+      },
+      error: error => {
+        console.warn('ERROR in 16: dialog-template.component.ts -.',error);
+      },
+    })
+  }
+
+}
---
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -0,0 +1,34 @@
+import { MessageService } from 'primeng/api';
+import { Component, OnInit} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { DialogExampleComponent } from './components/dialog-example/dialog-template.component';
+import { DialogService } from '@core/services/dialog.service';
+
+@Component({
+  selector: 'custom-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+  message = 'algo'
+  public show$: Observable<boolean>;
+
+  constructor(public dialog: MatDialog, public ds: DialogService, public messageService: MessageService) {
+    this.show$ = ds.DialogShowObservable;
+  }
+
+  ngOnInit(): void {
+    this.show$.subscribe( resp => { resp? this.openDialog(): null; } )
+  }
+
+  openDialog() {
+    let dialogRef = this.dialog.open(DialogExampleComponent, {
+      width: '30vw',
+      disableClose: true, //force disable with buttons
+    })
+    dialogRef.afterClosed().subscribe(result => {
+      this.ds.DialogSelection=result
+    });
+  }
+}
