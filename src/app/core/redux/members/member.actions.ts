fbb2aafe18a5bb01b48c91f3c4d9d637059427b0
frangdelsolar <frangdelsolar@gmail.com>
2022-03-09 20:27:39 -0300

    Members redux

---
+++ src/app/core/redux/members/member.actions.ts
@@ -0,0 +1,45 @@
+import { createAction, props } from '@ngrx/store';
+import { MembersResponse, MemberResponse, Member} from '@app/core/models/members.interfaces';
+import { Delete } from '@app/core/models/delete.interface';
+
+export enum type {
+  GET_MEMBERS = '[ Member ] Get All Members',
+  GET_MEMBERS_FAIL = '[ Member ] Get All Members Fail',
+  GET_MEMBERS_SUCCESS = '[ Member ] Get All Members Success',
+
+  GET_MEMBER = '[ Member ] Get A Member',
+  GET_MEMBER_FAIL = '[ Member ] Get A Member Fail',
+  GET_MEMBER_SUCCESS = '[ Member ] Get A Member Success',
+
+  POST_MEMBER = '[ Member ] Post A Member',
+  POST_MEMBER_FAIL = '[ Member ] Post A Member Fail',
+  POST_MEMBER_SUCCESS = '[ Member ] Post A Member Success',
+
+  UPDATE_MEMBER = '[ Member ] Update Member',
+  UPDATE_MEMBER_FAIL = '[ Member ] Update Member Fail',
+  UPDATE_MEMBER_SUCCESS = '[ Member ] Update Member Succes',
+
+  DELETE_MEMBER = '[ Member ] Delete Member',
+  DELETE_MEMBER_FAIL = '[ Member ] Delete Member Fail',
+  DELETE_MEMBER_SUCCESS = '[ Member ] Delete Member Success',
+}
+
+export const getMembers = createAction(type.GET_MEMBERS);
+export const getMembersSuccess = createAction(type.GET_MEMBERS_SUCCESS, props<{ members: MembersResponse }>());
+export const getMembersFail = createAction(type.GET_MEMBERS_FAIL, props<{ error: any }>());
+
+export const getMember = createAction(type.GET_MEMBER, props<{ id: number }>());
+export const getMemberSuccess = createAction(type.GET_MEMBER_SUCCESS, props<{ member: MemberResponse }>());
+export const getMemberFail = createAction(type.GET_MEMBER_FAIL, props<{error: any}>())
+
+export const postMember = createAction(type.POST_MEMBER, props<{ body: Member }>());
+export const postMemberSuccess = createAction(type.POST_MEMBER_SUCCESS, props<{ member: MemberResponse }>())
+export const postMemberFail = createAction(type.POST_MEMBER_FAIL, props<{error:any}>())
+
+export const updateMember = createAction(type.UPDATE_MEMBER, props<{  body: Member, id: number }>());
+export const updateMemberSuccess = createAction(type.UPDATE_MEMBER_SUCCESS, props<{ member: MemberResponse }>());
+export const updateMemberFail = createAction(type.UPDATE_MEMBER_FAIL, props<{ error: any }>());
+
+export const deleteMember = createAction(type.DELETE_MEMBER, props<{ id: number }>());
+export const deleteMemberSuccess = createAction(type.DELETE_MEMBER_SUCCESS, props<{ delete: Delete }>());
+export const deleteMemberFail = createAction(type.DELETE_MEMBER_FAIL, props<{ error: any }>());
---
+++ src/app/core/redux/members/member.reducer.ts
@@ -0,0 +1,41 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Delete } from '@app/core/models/delete.interface';
+import { createReducer, on } from '@ngrx/store';
+import * as action from '@app/core/redux/members/member.actions';
+import { Member, MemberResponse, MembersResponse } from '@app/core/models/members.interfaces';
+import { memberState } from '@app/core/models/member-state.interface';
+
+
+const memberEmpty: Member = { name: '' };
+const membersEmpty: Member[] = [{ name: '' }];
+const memberResponseEmpty: MemberResponse = { success: false, data: memberEmpty, message: '' };
+const membersResponseEmpty: MembersResponse = { success: false, data: membersEmpty, message: '' };
+const deleteEmpty: Delete = { success: false, data: '', message: '' };
+const errorEmpty = new HttpErrorResponse({});
+
+const initialState: memberState = {
+  responseAll: membersResponseEmpty,
+  response: memberResponseEmpty,
+  error: errorEmpty,
+  delete: deleteEmpty,
+};
+
+export const _memberReducer = createReducer(
+  initialState,
+  on(action.getMembersSuccess, (state,action) => {return {...state, responseAll: action.members}}),
+  on(action.getMembersFail, (state,action)=> {return {...state,error: action.error}}),
+
+  on(action.getMemberSuccess, (state,action) => {return {...state, response: action.member}}),
+  on(action.getMemberFail, (state,action) => {return {...state, error: action.error}}),
+
+  on(action.updateMemberSuccess,(state,action) => {return {...state,response: action.member}}),
+  on(action.updateMemberFail, (state,action) => {return {...state,error: action.error}}),
+
+  on(action.deleteMemberSuccess,(state,action) => {return {...state,delete: action.delete}}),
+  on(action.deleteMemberFail,(state,action) => {return {...state,error: action.error}})
+
+);
+
+export function memberReducer(state,action){
+  return _memberReducer(state,action);
+}
