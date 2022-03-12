c63ca24e1d09d20c6386ed2682a02e7903d424e3
Facundo Saldivia <50809738+FacundoSaldivia@users.noreply.github.com>
2022-03-12 13:27:52 -0300

    Ot153 126 header (#80)
    * link interface created and mobile header created
    
    * header completed
    
    * loadLinks function modified
    
    * new styles to global header added
    
    * navbar styles modified - padding added to hamburger btn
    
    Co-authored-by: Ricardo Maldonado <maldonadoricardo93@gmail.com>

--- src/app/core/components/header/header/header.component.ts
+++ src/app/core/components/header/header/header.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { link } from '@app/core/models/link.interface';
+import { AuthState } from '@app/core/redux/auth/auth.reducers';
+import { getAuth } from '@app/core/redux/auth/auth.selectors';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'alk-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor(private Store: Store<AuthState>) {
+     this.authentication$ = this.Store.pipe(select(getAuth));
+  }
+  authentication$: Observable<boolean>;
+  links: link[];
+  linksHome: link[] = [
+    { link: '/home', text: 'Inicio' },
+    { link: '/nosotros', text: 'Nosotros' },
+    { link: '/contacto', text: 'Contacto' },
+  ];
+  linksBackoffice: link[] = [
+    { link: '/backoffice/organization', text: 'Organizacion' },
+    { link: '/backoffice/slides', text: 'Slides' },
+    { link: '/backoffice/actividades', text: 'Actividades' },
+    { link: '/backoffice/users', text: 'Usuarios' },
+    { link: '/backoffice/members', text: 'Miembros' },
+    { link: '/backoffice/categorias', text: 'Usuarios' },
+    { link: '/backoffice/', text: 'Dashboard' },
+  ];
+  openNavbar: boolean = false;
 
   ngOnInit(): void {
+    this.loadLinks();
   }
 
+  loadLinks(): void {
+    this.authentication$.subscribe(auth =>{
+      if (auth) {
+        this.links = [...this.linksHome, ...this.linksBackoffice];
+      } else {
+        this.links = this.linksHome;
+      }
+    })
+ 
+  }
 }
