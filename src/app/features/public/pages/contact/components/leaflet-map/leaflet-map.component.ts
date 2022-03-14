ed26e578698a379aed182d392e405b919b811ea3
NLuna19 <nluna190898@gmail.com>
2022-03-14 12:01:31 -0300

    added markers, layerControl

--- src/app/features/public/pages/contact/components/leaflet-map/leaflet-map.component.ts
+++ src/app/features/public/pages/contact/components/leaflet-map/leaflet-map.component.ts
@@ -1,5 +1,8 @@
+import { DialogService } from '@app/core/services/dialog.service';
 import { Component, OnInit } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
+import { icon, latLng, Layer, Map, marker, point, tileLayer } from 'leaflet';
+import { Subject } from 'rxjs';
+import { debounceTime, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-leaflet-map',
@@ -8,15 +11,50 @@ import { latLng, tileLayer } from 'leaflet';
 })
 export class LeafletMapComponent implements OnInit {
 
-  options = {
-    layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909)
+  // Define our base layers
+  wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    attribution: '&copy; <a href="">WikimediaMap</a> contributors'
+  });
+
+  streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  // Marker
+  organization = marker([4.668740392164804, -74.0620359551276], { //???
+    icon: icon({
+      iconSize: [ 25, 41 ],
+      iconAnchor: [ 13, 41 ],
+      iconUrl: 'leaflet/marker-icon.png',
+      shadowUrl: 'leaflet/marker-shadow.png'
+    })
+  });
+  
+  // Layers control object, base layers and overlay layers
+  layersControl = {
+    baseLayers: {
+      'Wikimedia Maps': this.wMaps,
+      'OpenStreet Maps': this.streetMaps
+    },
+    overlays: {
+      'Somos Mas': this.organization,
+    }, 
   };
 
-  constructor() { }
+  // Options
+  options = {
+    layers: [ this.wMaps, this.organization ],
+    zoom: 14,
+    center: latLng([4.668740392164804, -74.0620359551276]) //???
+  };
+
+	constructor( public dialog: DialogService) {
+	}
+
+  onMapReady(map: Map) {
+  }
 
   ngOnInit() {
   }
