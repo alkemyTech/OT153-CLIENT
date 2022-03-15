c403a29788f87c6989e096b90757ac1f939149c1
NLuna19 <nluna190898@gmail.com>
2022-03-15 11:54:39 -0300

    deletion of import and addition of coordinates in variable

--- src/app/features/public/pages/contact/components/leaflet-map/leaflet-map.component.ts
+++ src/app/features/public/pages/contact/components/leaflet-map/leaflet-map.component.ts
@@ -1,8 +1,6 @@
 import { DialogService } from '@app/core/services/dialog.service';
 import { Component, OnInit } from '@angular/core';
-import { icon, latLng, Layer, Map, marker, point, tileLayer } from 'leaflet';
-import { Subject } from 'rxjs';
-import { debounceTime, scan } from 'rxjs/operators';
+import { icon, latLng, LatLngTuple, Map, marker, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-leaflet-map',
@@ -10,6 +8,7 @@ import { debounceTime, scan } from 'rxjs/operators';
   styleUrls: ['./leaflet-map.component.scss']
 })
 export class LeafletMapComponent implements OnInit {
+  private coordOrganization :LatLngTuple= [4.668740392164804, -74.0620359551276];
 
   // Define our base layers
   wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
@@ -23,7 +22,7 @@ export class LeafletMapComponent implements OnInit {
   });
 
   // Marker
-  organization = marker([4.668740392164804, -74.0620359551276], { //???
+  organization = marker(this.coordOrganization, { //???
     icon: icon({
       iconSize: [ 25, 41 ],
       iconAnchor: [ 13, 41 ],
@@ -47,15 +46,12 @@ export class LeafletMapComponent implements OnInit {
   options = {
     layers: [ this.wMaps, this.organization ],
     zoom: 14,
-    center: latLng([4.668740392164804, -74.0620359551276]) //???
+    center: latLng(this.coordOrganization) //???
   };
 
-	constructor( public dialog: DialogService) {
+	constructor() {
 	}
-
-  onMapReady(map: Map) {
-  }
-
+  
   ngOnInit() {
   }
 
