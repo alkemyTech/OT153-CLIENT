f8bf32f25ece76a7c1e90557400b172578e9dc4f
federicorojas11 <federico.rojas.121@gmail.com>
2022-03-16 10:37:09 -0300

    added api call for social links on footer

--- src/app/core/services/publicApi.service.ts
+++ src/app/core/services/publicApi.service.ts
@@ -1,19 +1,25 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { ActivityResponse } from "../models/activities.interfaces";
-import { Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivityResponse } from '../models/activities.interfaces';
+import { Observable } from 'rxjs';
+import { Organization } from '../models/organization.interfaces';
+import { environment } from '@env/environment';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class PublicapiService {
   constructor(private http: HttpClient) {}
 
   public get<T>(url: string, id: number | null = null): Observable<T> {
-    return this.http.get<T>(`${url}/${id ? id : ""}`);
+    return this.http.get<T>(`${url}/${id ? id : ''}`);
   }
 
   public getPublicActivity<T>(url: string, id: string | number | null = null): Observable<ActivityResponse> {
-    return this.http.get<ActivityResponse>(`${url}/${id ? id : ""}`);
+    return this.http.get<ActivityResponse>(`${url}/${id ? id : ''}`);
+  }
+
+  public getPublicOrganization<T>(): Observable<Organization> {
+    return this.http.get<Organization>(environment.apiUrlOrganization);
   }
 }
