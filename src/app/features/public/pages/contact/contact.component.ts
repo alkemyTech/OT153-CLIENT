ee78377f12f0e123f3a066d590cf52c24d3b616b
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2022-03-16 11:10:36 -0300

    fixing errors alert

--- src/app/features/public/pages/contact/contact.component.ts
+++ src/app/features/public/pages/contact/contact.component.ts
@@ -3,7 +3,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Organization } from '@app/core/models/organization.interfaces';
 import { HttpService } from '@app/core/services/http.service';
 import { PublicapiService } from '@app/core/services/publicApi.service';
-
+import { environment } from '@env/environment';
+import { DialogService } from '@app/core/services/dialog.service';
+import { DialogData } from '@app/core/models/dialog.inteface';
+import { DialogType } from '@app/core/enums/dialog.enum';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -12,9 +15,9 @@ import { PublicapiService } from '@app/core/services/publicApi.service';
 export class ContactComponent implements OnInit {
   title: string = 'Contactate con nosotros';
   colorBackground: string = 'transparent';
-  organization_link: string = 'api/organization';
+  organization_link: string = environment.apiUrlOrganization;
   organization_info: Organization;
-  constructor(public http: PublicapiService) {}
+  constructor(public http: PublicapiService, private dialogService: DialogService) {}
 
   ngOnInit(): void {
     this.getContactInfo();
@@ -26,7 +29,8 @@ export class ContactComponent implements OnInit {
         this.organization_info = res;
       },
       (error) => {
-        alert(error);
+        let dialog: DialogData = { type: DialogType.ERROR, header:  'Error al procesar la operación', content: 'Los datos de la organización no pudieron ser cargados.'};
+        this.dialogService.show(dialog);
       }
     );
   }
