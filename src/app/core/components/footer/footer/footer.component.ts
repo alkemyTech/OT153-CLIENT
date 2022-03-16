19a22bae6beadaa940913ec079ef146e4972d783
federicorojas11 <47337947+federicorojas11@users.noreply.github.com>
2022-03-16 13:02:25 -0300

    Ot153 127 create footer public component (#97)
    * added footer component styles & logo and navigation
    
    * added api call for social links on footer
    
    * added social links basic styles
    
    * changed footer bg color

--- src/app/core/components/footer/footer/footer.component.ts
+++ src/app/core/components/footer/footer/footer.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PublicapiService } from '@app/core/services/publicApi.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'alk-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
   date: Date = new Date();
-  constructor() { }
+  facebookLink: string;
+  instagramLink: string;
+  twitterLink: string;
+  subscription: Subscription;
+
+  constructor(private publicService: PublicapiService) {}
 
   ngOnInit(): void {
+    this.subscription = this.publicService.getPublicOrganization().subscribe((organization) => {
+      this.facebookLink = organization.data.facebook_url;
+      this.instagramLink = organization.data.instagram_url;
+      this.twitterLink = organization.data.twitter_url;
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
