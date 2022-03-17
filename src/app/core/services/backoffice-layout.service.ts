2ae8007054955a7d8959e4df22f1f1b4ea9797e4
NLuna19 <nluna190898@gmail.com>
2022-03-17 11:52:05 -0300

    added backoffice service

---
+++ src/app/core/services/backoffice-layout.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackofficeLayoutService {
+
+  private initialBackofficeLayout = {
+    actual: '',
+    showSlide: false, 
+  }
+
+  private actualObservable: BehaviorSubject<string> = new BehaviorSubject<string>(
+    this.initialBackofficeLayout.actual );
+  private showSlideObservable: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    this.initialBackofficeLayout.showSlide );
+  
+  constructor() {}
+  
+  get LayoutActualObservable(): Observable<string> {
+      return this.actualObservable.asObservable()
+  }
+  
+  get ShowSlideObservable(): Observable<boolean> {
+      return this.showSlideObservable.asObservable()
+  }
+
+  set LayoutActual( nuevoactual : string ){
+    this.actualObservable.next(nuevoactual);
+  }
+
+  private set ShowSlide(flag : boolean) {
+    this.showSlideObservable.next(flag);
+  }
+
+  showSlide() : void{
+    this.ShowSlide = true;
+    console.log('show');
+  }
+
+  hideSlide() : void{
+    this.ShowSlide = false;
+    console.log('hide');
+  }
+
+    
+}
