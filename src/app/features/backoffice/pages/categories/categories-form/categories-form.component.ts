6976a0d5efab554d4db5ac4c5813d74d081ad012
Facundo Saldivia <facundosaldivia@gmail.com>
2022-03-23 20:13:32 -0300

    categories form started

--- src/app/features/backoffice/pages/categories/categories-form/categories-form.component.ts
+++ src/app/features/backoffice/pages/categories/categories-form/categories-form.component.ts
@@ -3,6 +3,9 @@ import { Store } from '@ngrx/store';
 import { fullCategoryDate } from '@core/models/category.interface';
 import { listCategories } from '@core/redux/categories/categories.actions';
 import { SelectAllCategories } from '@core/redux/categories/categories.selector'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { getControl as getControlFunction } from '@app/core/util/getControlForm';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-categories-form',
@@ -10,22 +13,40 @@ import { SelectAllCategories } from '@core/redux/categories/categories.selector'
   styleUrls: ['./categories-form.component.scss'],
 })
 export class CategoriesFormComponent implements OnInit {
-
   categories: fullCategoryDate[];
   loading: boolean = false;
   loaded: boolean = false;
   error: any = null;
+  newForm: boolean = true;
+  private classicEditor = ClassicEditor;
+  public form: FormGroup;
+  public name: FormControl = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  public description: FormControl = new FormControl('', [Validators.required]);
+  public image: FormControl = new FormControl('', [Validators.required]);
+  public getControl = getControlFunction;
 
-  constructor(private store: Store<any>) {}
+  constructor(private store: Store<any>, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.store.select(SelectAllCategories).subscribe( ({categories, error, loaded, loading}) => {
+    this.store.select(SelectAllCategories).subscribe(({ categories, error, loaded, loading }) => {
       this.categories = categories;
       this.loading = loading;
       this.loaded = loaded;
       this.error = error;
-    })
+    });
 
-    this.store.dispatch(listCategories())
+    this.store.dispatch(listCategories());
+  }
+
+  createForm(): FormGroup {
+    return this.formBuilder.group({
+      name: this.name,
+      description: this.description,
+      image: this.image,
+    });
+  }
+
+  submit(): void {
+    console.log(this.form.value);
   }
 }
