3249bbefe201c1024627dad7d6500b9a9670cb12
frangdelsolar <frangdelsolar@gmail.com>
2022-03-24 11:45:33 -0300

    test form component

--- src/app/features/backoffice/pages/activities/activity-form/activity-form.component.spec.ts
+++ src/app/features/backoffice/pages/activities/activity-form/activity-form.component.spec.ts
@@ -1,14 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActivityFormComponent } from './activity-form.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
+import { HttpErrorResponse } from '@angular/common/http';
+import { of } from 'rxjs';
 
-describe('ActivityFormComponent', () => {
+
+let authState = {
+  auth: false,
+  isAdmin: false,
+  user: null,
+  googleUser: null,
+  token!: null,
+  isGoogleAuth: false
+}
+
+
+describe('Testing Activity Form Component', () => {
   let component: ActivityFormComponent;
   let fixture: ComponentFixture<ActivityFormComponent>;
+  let element;
+
+  let activitiesState = {
+    responseAll: [],
+    response: {id: 0, name: '', description:''},
+    delete:  { success: false , data: '', message: '' },
+    error: new HttpErrorResponse({}),
+  }
+
+  const storeMock = jasmine.createSpyObj('store', ['select']);
+
 
   beforeEach(async () => {
+    storeMock.select.and.returnValue(
+      of(activitiesState)
+    );
+
     await TestBed.configureTestingModule({
       declarations: [ActivityFormComponent],
+      imports: [RouterTestingModule, StoreModule.forRoot({}, {}),],
+      providers: [{ provide: Store, useValue: storeMock }],
     }).compileComponents();
   });
 
@@ -18,7 +51,36 @@ describe('ActivityFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load three fields', () =>{
+    expect( component.form.contains('name')).toBeTruthy();
+    expect( component.form.contains('image')).toBeTruthy();
+    expect( component.form.contains('description')).toBeTruthy();
+  });
+
+  it('Field name should be mandatory', () =>{
+    const control = component.form.get('name');
+    control?.setValue('');
+   
+    expect( control?.valid ).toBeFalsy();
+  });
+
+  // it('should upload the file', () => {
+  //   component.selectFile(new Event('change'));
+  //   const inputEl = element.querySelector('#image_file');
+  //   const fileList = { 0: { name: 'foo', size: 500001 } };
+  //   inputEl.value = {
+  //       target: {
+  //           files: fileList
+  //       }
+  //   };
+  //   inputEl.dispatchEvent(new Event('change'));     
+  // });
+
+  it('Only logged user should be able to navigate form', () =>{
+
   });
 });
