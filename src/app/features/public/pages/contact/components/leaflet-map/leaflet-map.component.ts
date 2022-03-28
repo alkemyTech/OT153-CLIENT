1e5d612e1bc177386326b4712e602c357262bc49
Nicolas Omar Luna <nluna190898@gmail.com>
2022-03-28 13:30:42 -0300

    Ot153 fix map contact (#140)
    * changed leaflet to google map
    
    * changed style

--- src/app/features/public/pages/contact/components/leaflet-map/leaflet-map.component.ts
+++ src/app/features/public/pages/contact/components/leaflet-map/leaflet-map.component.ts
@@ -15,7 +15,8 @@ export class LeafletMapComponent implements OnInit {
   public mapOption: L.MapOptions;
 
 	constructor() {
-    this.initMap();
+    // this.initMap();
+
 	}
   
   ngOnInit() {
@@ -55,5 +56,44 @@ export class LeafletMapComponent implements OnInit {
     .on( 'mouseover',function(ev:L.LeafletEvent) { ev.target.openPopup(); } )
     .on( 'mouseout',function(ev:L.LeafletEvent) { ev.target.closePopup(); } );
   }
+
+  mapOptions = {
+    zoom: 10,
+    mapId: 'fadc1ec7936b54d3'
+  };
+
+  position = {
+    lat: 4.668740392164804,
+    lng: -74.0620359551276,
+  }; 
+
+  label = {
+    color: 'navy',
+    text: "SOMOS MAS",
+  };
+
+  icon = {
+    url: "assets/map/marker.png", 
+    scaledSize: new google.maps.Size(30, 43), 
+    origin: new google.maps.Point(0,0), 
+    anchor: new google.maps.Point(0, 0),
+    labelOrigin: new google.maps.Point(0, 0),
+  };
+
+  svgMarker = {
+    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+    fillColor: "blue",
+    fillOpacity: 1,
+    strokeWeight: 0,
+    rotation: 0,
+    scale: 3,
+    anchor: new google.maps.Point(14, 24),
+  };
+
+  markerOptions: google.maps.MarkerOptions = {
+    draggable: false,
+    icon: this.svgMarker,
+  };
+  
 
 }
