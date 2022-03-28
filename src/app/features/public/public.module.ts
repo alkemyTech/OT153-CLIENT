0f0fd72da8e5a7905e846d2c70795b0c6865e8df
Facundo Saldivia <50809738+FacundoSaldivia@users.noreply.github.com>
2022-03-28 13:29:24 -0300

    new header added to back-to-school campaign (#138)
    Co-authored-by: Ricardo Maldonado <maldonadoricardo93@gmail.com>

--- src/app/features/public/public.module.ts
+++ src/app/features/public/public.module.ts
@@ -26,6 +26,7 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { CampaignsSchoolComponent } from './pages/campaigns/back-to-school/campaigns-school.component';
 import { ToysComponent } from './pages/campaigns/toys/toys.component';
 import { ToysHeaderComponent } from './pages/campaigns/toys/components/header/header.component';
+import { BackToSchoolHeader } from './pages/campaigns/back-to-school/components/header/header.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 
@@ -55,6 +56,7 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
     LeafletMapComponent,
     ToysComponent,
     ToysHeaderComponent,
+    BackToSchoolHeader,
     PageNotFoundComponent,
   ],
   imports: [CommonModule, PublicRoutingModule, SharedModule, LeafletModule],
