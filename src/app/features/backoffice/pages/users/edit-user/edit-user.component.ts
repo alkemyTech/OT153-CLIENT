12c710d242ad86c5c948865a231096230fabb1fb
federico.rojas.121 <federico.rojas.121@gmail.com>
2022-03-28 18:38:13 -0300

    added edit user by id route & get user

--- src/app/features/backoffice/pages/users/edit-user/edit-user.component.ts
+++ src/app/features/backoffice/pages/users/edit-user/edit-user.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { UserControllerService } from '@app/core/controllers/user-controller.service';
+import { userState } from '@app/core/models/user-state.interface';
+import { User } from '@app/core/models/user.models';
+import * as userActions from '@app/core/redux/users/user.actions';
+import * as userSelector from '@app/core/redux/users/user.selector';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.scss'],
 })
-export class EditUserComponent implements OnInit {
+export class EditUserComponent implements OnInit, OnDestroy {
   submitted: boolean = false;
   form: FormGroup = new FormGroup(
     {
@@ -15,17 +22,44 @@ export class EditUserComponent implements OnInit {
     },
     { updateOn: 'change' }
   );
+  user$: Observable<any>;
+  username: string = '';
+  email: string = '';
+  userSubscribe: Subscription;
+  _id: number | undefined = 0;
+  constructor(
+    private router: Router,
+    private userService: UserControllerService,
+  ) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.getUser();
+  }
 
-  ngOnInit(): void {}
+  getUser() {
+    this._id = parseFloat(this.router.url.split('/').pop() + '');
+    this.userService.getUserById(this._id).subscribe({
+      next: (user) => {
+        this.form.controls['email'].patchValue(user.data.email);
+        this.form.controls['name'].patchValue(user.data.name);
+        console.log('next', user);
+      },
+      error: (error) => {
+        error;
+      },
+    });
+  }
 
   onSubmit(form: FormGroup) {
     this.submitted = true;
     if (form.valid) {
+
   //post
-    }
+
+}
   }
 
-
+  ngOnDestroy(): void {
+    this.userSubscribe.unsubscribe();
+  }
 }
