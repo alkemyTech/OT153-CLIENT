1e2db3310bb9ee3397cd44fc792c8b06bc005eb9
Facundo Saldivia <50809738+FacundoSaldivia@users.noreply.github.com>
2022-03-29 13:27:34 -0300

    Home styling changes (#146)
    * modificaciones al home
    
    * home styling improved

--- src/app/features/public/pages/home/home.component.ts
+++ src/app/features/public/pages/home/home.component.ts
@@ -10,33 +10,43 @@ import { timer } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   load: boolean = true;
-  public title:string = 'Somos Más ONG';
-  public description:string = 'Somos Más es una organización que trabaja para dar respuesta a las problemáticas sociales que derivan de la pobreza.';
+  public title: string = 'Somos Más ONG';
+  public description: string =
+    'Somos una ONG dedicada a combatir la pobreza.  Por eso nuestro trabajo está orientado a acabar con las desigualdades sociales y económicas, que son el origen del problema. Queremos un futuro sin pobreza y caminamos decididamente hacia él.';
   public urlSlides = environment.apiUrlSlides;
   public slides: SlideData[];
-  constructor(private privateService: PrivateService, private dialogService: DialogService, private progressbarService: ProgressBarService) { }
+  constructor(
+    private privateService: PrivateService,
+    private dialogService: DialogService,
+    private progressbarService: ProgressBarService
+  ) {}
 
   ngOnInit(): void {
     this.progressbarService.setDisplay(true);
     this.getSlides();
-}
-  getSlides(){
-    this.privateService.get<Slides>(this.urlSlides).subscribe((response) => {
-      this.slides = response.data;
-      this.slides = this.slides.slice(0, 5);
-      this.load = false;
-      this.progressbarService.setDisplay(false);
-  },error=>{
-    this.load = false;
-    let dialog: DialogData = { type: DialogType.ERROR, header:  'Error al procesar la operación', content: 'El Carousel de imagenes no pudo ser cargado.'};
-    this.dialogService.show(dialog);
-    this.progressbarService.setDisplay(false);
-  });
   }
-
-
+  getSlides() {
+    this.privateService.get<Slides>(this.urlSlides).subscribe(
+      (response) => {
+        this.slides = response.data;
+        this.slides = this.slides.slice(0, 5);
+        this.load = false;
+        this.progressbarService.setDisplay(false);
+      },
+      (error) => {
+        this.load = false;
+        let dialog: DialogData = {
+          type: DialogType.ERROR,
+          header: 'Error al procesar la operación',
+          content: 'El Carousel de imagenes no pudo ser cargado.',
+        };
+        this.dialogService.show(dialog);
+        this.progressbarService.setDisplay(false);
+      }
+    );
+  }
 }
